b29e27b8f550460640c5f072dd51e0550917c2b9
YJ Soon <yjsoon@tinkertanker.com>
2020-12-11 11:36:32 +0800

    Set up textinput in the add screen

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -1,21 +1,38 @@
-import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+} from "react-native";
 
 export default function AddScreen({ navigation }) {
+  const [text, setText] = useState("");
+
   return (
     <View style={[styles.container, { backgroundColor: "white" }]}>
-      <Text style={{ fontSize: 24 }}>This is the add screen</Text>
-      <TouchableOpacity
-        style={{
-          padding: 10,
-          backgroundColor: "orange",
-          borderRadius: 5,
-          marginTop: 30,
-        }}
-        onPress={() => navigation.goBack()}
-      >
-        <Text>Dismiss</Text>
-      </TouchableOpacity>
+      <Text style={{ fontSize: 24 }}>What do you want to add?</Text>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={(input) => setText(input)}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.goBack()}
+        >
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.goBack()}
+        >
+          <Text style={styles.buttonText}>Dismiss</Text>
+        </TouchableOpacity>
+      </View>
+      {/* <Text>{text.toUpperCase()}</Text> */}
     </View>
   );
 }
@@ -26,5 +43,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffc",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textInput: {
+    borderColor: "grey",
+    borderWidth: 1,
+    width: "80%",
+    padding: 10,
+    marginTop: 20,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "orange",
+    borderRadius: 5,
+    margin: 10,
+    marginTop: 30,
+    width: 80,
+  },
+  buttonText: {
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
   },
 });
