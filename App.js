80dbee3058e406452f701bdd8f83f71843c4d434
YJ Soon <yjsoon@tinkertanker.com>
2020-12-11 11:15:34 +0800

    Refactored out NotesScreen and NotesStack

--- App.js
+++ App.js
@@ -1,111 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  FlatList,
-} from "react-native";
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
 import * as SQLite from "expo-sqlite";
 import AddScreen from "./screens/AddScreen";
+import NotesStack from "./screens/NotesStack";
 
 const db = SQLite.openDatabase("notes.db");
-
-const SAMPLE_NOTES = [
-  { title: "Feed the monkey", done: false, id: "0" },
-  { title: "Do the laundry", done: false, id: "1" },
-  { title: "More sample data", done: false, id: "2" },
-];
-
-function NotesScreen({ navigation }) {
-  const [notes, setNotes] = useState(SAMPLE_NOTES);
-
-  // This is to set up the top right button
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity onPress={addNote}>
-          <Ionicons
-            name="ios-create-outline"
-            size={30}
-            color="black"
-            style={{
-              color: "#f55",
-              marginRight: 10,
-            }}
-          />
-        </TouchableOpacity>
-      ),
-    });
-  });
-
-  function addNote() {
-    navigation.navigate("Add Screen");
-    // const newNote = {
-    //   title: "Sample note",
-    //   done: false,
-    //   id: notes.length.toString(),
-    // };
-    // setNotes([...notes, newNote]);
-  }
-
-  function renderItem({ item }) {
-    return (
-      <View
-        style={{
-          padding: 10,
-          paddingTop: 20,
-          paddingBottom: 20,
-          borderBottomColor: "#ccc",
-          borderBottomWidth: 1,
-        }}
-      >
-        <Text>{item.title}</Text>
-      </View>
-    );
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={notes}
-        renderItem={renderItem}
-        style={{ width: "100%" }}
-      />
-    </View>
-  );
-}
-
-const InnerStack = createStackNavigator();
-function NotesStack() {
-  return (
-    <InnerStack.Navigator>
-      <InnerStack.Screen
-        name="Notes"
-        component={NotesScreen}
-        options={{
-          title: "Notes, a Todo App",
-          headerStyle: {
-            backgroundColor: "yellow",
-            height: 100,
-            shadowColor: "black",
-            shadowOpacity: 0.2,
-            shadowRadius: 5,
-          },
-          headerTintColor: "#f55",
-          headerTitleStyle: {
-            fontSize: 24,
-            fontWeight: "bold",
-          },
-        }}
-      />
-    </InnerStack.Navigator>
-  );
-}
 
 const Stack = createStackNavigator();
 export default function App() {
@@ -118,12 +18,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffc",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/NotesScreen.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const SAMPLE_NOTES = [
+  { title: "Feed the monkey", done: false, id: "0" },
+  { title: "Do the laundry", done: false, id: "1" },
+  { title: "More sample data", done: false, id: "2" },
+];
+
+export default function NotesScreen({ navigation }) {
+  const [notes, setNotes] = useState(SAMPLE_NOTES);
+
+  // This is to set up the top right button
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addNote}>
+          <Ionicons
+            name="ios-create-outline"
+            size={30}
+            color="black"
+            style={{
+              color: "#f55",
+              marginRight: 10,
+            }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
+  function addNote() {
+    navigation.navigate("Add Screen");
+    // const newNote = {
+    //   title: "Sample note",
+    //   done: false,
+    //   id: notes.length.toString(),
+    // };
+    // setNotes([...notes, newNote]);
+  }
+
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text>{item.title}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={notes}
+        renderItem={renderItem}
+        style={{ width: "100%" }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
